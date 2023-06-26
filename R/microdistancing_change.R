b5b3cb05d9216cbf3b4368ea7c6412577c98f5ef
SeniorKate <senior.k@unimelb.edu.au>
2023-06-26 17:15:31 +1000

    Minor change to x-axis of microdistancing trend plot (full range)

--- R/microdistancing_change.R
+++ R/microdistancing_change.R
@@ -233,7 +233,7 @@ p <- ggplot(line_df) +
   
   coord_cartesian(ylim = c(0, 100)) +
   scale_y_continuous(position = "right") +
-  scale_x_date(date_breaks = "2 month", date_labels = "%e/%m") +
+  scale_x_date(date_breaks = "3 month", date_labels = "%e/%m") +
   scale_alpha(range = c(0, 0.5)) +
   scale_fill_manual(values = c("Nowcast" = base_colour)) +
   
